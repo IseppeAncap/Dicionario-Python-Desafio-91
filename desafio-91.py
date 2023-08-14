3047ae0e7dc3bddf62c9ffe9a079c1ebcefb1a3e
IseppeAncap <141202141+IseppeAncap@users.noreply.github.com>
2023-08-14 14:46:59 -0300

    Update desafio-91.py
    Retirei o passo a passo ,optei pro deixar apenas no arquivo README.md

--- desafio-91.py
+++ desafio-91.py
@@ -1,12 +1,4 @@
-'''
-Objetivo 91 
-    Crie um programa onde 4ª jogadores, joguem um dado obtenham resultados
-aléatorios.
-Guarde esses resultados em um dicionário.
-No final coloque esse dicionário em ordem decrescente .
-Sabendo que o vencêndor tirou o maior número do dado.
 
-'''
 # Definição de cores para outupt
 reset = '\033[0m'
 preto = '\033[30m'
@@ -61,38 +53,3 @@ for resultado in sorted(sorteio, key=sorteio.get, reverse=True):
 
 print(f'Obrigado Pro usar nosso Programa!')
 print('-=-=-'*10)
-'''
-Passo a passo
-# Etapa 1 - Definindo algumas estruturas e variavéis
-
-    .Importar a bilbioteca random e time.
-    .Criar um diconário "sorteio",que será atualizado,a partir de outro dicionário "dados".
-    .Usáremos um contador para exibir,o valor da posição ao final.
-
-## Etapa 2 -Preenchendo o dicionário com Valores Sorteados
-    .Usaremos um laço até 4 jogadores
-    .Como o nome da chave/key será jogador1, teremos que converter o tipo da variavél e concatena-la 
-e guardar esse valor.
-    .utilizamos o método randinting para,simular um sorteio de um dado, aṕos isso adiciona esse valor depois do nome
-chave/key.
-    .Atualizamos o valor do dicionario "sorteio" a partir do dict"dados"
-----------------------------------------------------------------------
-## Etapa 3 - Exibir o sorteio.
-    .Usamos um laço que itera sobre o itens key/values do {sorteio}
-    .Exibimos menagen "jogador1 tirou 4"
-    .Usamos o método sleep(.5) para pausar entre essas iterções.
-## Etapa 4 - Exibindo um Ranking, de acordo com resultados do sorteio em ordem 
-decrescente !
-. Começamos exibindo uma mensagem  (::::RANKING:::::)
-
-. Decidi usar um laço for para percorrer cada elemento,antes começaremos na mesma linha de códido 
-usar método sort com parâmetro key que especifica a função usada para extrair 
-uma chave de comparação dos elementos. 
-Nesse caso, sorteio.get é usado para obter o valor associado a cada chave. 
-O parâmetro reverse=True faz com que a ordenação seja feita em ordem decrescente.
-Saida
-    . Usando o Contador para mostrar a posição do Podion, icrementamos +1 a cada iteração resultado
-    .Exibimos a posição chave  e o valor
-    .Usamos também o metodo sleep para pausar a cada mensagem
-    .Impletamos uso de teste de dedicão para imprimir a mensagem com emojis 
-'''
